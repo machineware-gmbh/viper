72ba28054f327112b4a3e4a01bb39452e41a9846
Jan Henrik Weinstock <jan.weinstock@rwth-aachen.de>
2018-08-28 13:47:03 +0200

    Adding service for module inspection

---
+++ bundles/org.vcml.explorer.ui/src/org/vcml/explorer/ui/services/IInspectionService.java
@@ -0,0 +1,46 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright 2018 Jan Henrik Weinstock                                        *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ *     http://www.apache.org/licenses/LICENSE-2.0                             *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ *                                                                            *
+ ******************************************************************************/
+
+package org.vcml.explorer.ui.services;
+
+import org.vcml.session.Module;
+
+public interface IInspectionService {
+
+    /**
+     * Register a module kind to be inspected by the provided view part
+     * @param kind  Kind of module to inspect (processor, bus, memory...)
+     * @param part  Class name of part that provides the UI
+     */
+    public void registerModulePart(String kind, String part);
+
+    /**
+     * Returns whether any UI part is available to inspect the specified module
+     * @param module    Module to inspect
+     * @return          <tt>true</tt> if <tt>module</tt> can be inspected
+     */
+    public boolean isInspectable(Module module);
+
+    /**
+     * Returns the URI of the part that handles the inspection of the specified module.
+     * @param module    Module to inspect
+     * @return          URI of the UI part that handles inspection or <tt>null</tt>
+     */
+    public String lookupPartContributionURI(Module module);
+
+}
---
+++ bundles/org.vcml.explorer.ui/src/org/vcml/explorer/ui/services/InspectionService.java
@@ -0,0 +1,56 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright 2018 Jan Henrik Weinstock                                        *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ *     http://www.apache.org/licenses/LICENSE-2.0                             *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ *                                                                            *
+ ******************************************************************************/
+
+package org.vcml.explorer.ui.services;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.vcml.session.Module;
+
+
+public class InspectionService implements IInspectionService {
+
+    public static final String BUNDLE_URI = "bundleclass://org.vcml.explorer.ui";
+
+    private Map<String, String> parts = new HashMap<String, String>();
+
+    @Override
+    public void registerModulePart(String kind, String part) {
+        String uri = BUNDLE_URI + "/" + part;
+        parts.put(kind, uri);
+    }
+
+    @Override
+    public boolean isInspectable(Module module) {
+        return parts.containsKey(module.getKind());
+    }
+
+    @Override
+    public String lookupPartContributionURI(Module module) {
+        return parts.get(module.getKind());
+    }
+
+    public InspectionService() { // find a better way to do this
+        registerModulePart(Module.KIND_VCML_PROCESSOR, "org.vcml.explorer.ui.parts.SystemCPart");
+        registerModulePart(Module.KIND_VCML_MEMORY, "org.vcml.explorer.ui.parts.SystemCPart");
+        registerModulePart(Module.KIND_VCML_BUS, "org.vcml.explorer.ui.parts.SystemCPart");
+        registerModulePart(Module.KIND_VCML_UART8250, "org.vcml.explorer.ui.parts.SystemCPart");
+    }
+
+}
